e732d4bc55517516a5d20c09760bcf8c796ac568
anton <anthony.uvarenko@gmail.com>
2023-10-19 19:19:07 +0300

    feat: lesson7 pz_1

---
+++ lesson7/pz_1.js
@@ -0,0 +1,19 @@
+const books = ['Harry Potter', 'Witcher', '1984'];
+console.log(books);
+
+books.sort();
+console.log(books);
+
+books.pop();
+console.log(books);
+
+books.unshift('Game of thrones');
+console.log(books);
+
+books.shift();
+console.log(books);
+
+books.splice(1, 1, 'Hobbit');
+console.log(books);
+
+console.log(books.includes('Harry Potter'));
