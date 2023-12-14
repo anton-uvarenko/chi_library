22997cb9e4fd47359c07bc868214c9c4d8954d39
anton <anthony.uvarenko@gmail.com>
2023-12-14 19:55:15 +0200

    feat: middleware strictly typed

--- with_redux/my-app/src/middlewares/logger.ts
+++ with_redux/my-app/src/middlewares/logger.ts
@@ -1,4 +1,7 @@
-export const loggerMiddleware = (sotre: any) => (next: any) => (action: any) => {
+import {Middleware} from "@reduxjs/toolkit";
+
+
+export const loggerMiddleware: Middleware = storeApi => next => action => {
     console.log("action type", action.type, "action", action);
     next(action);
 }
